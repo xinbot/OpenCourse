adcf28285926bf989034652c67644d4c4a1f84b5
Yum Lam <linxin025@gmail.com>
2022-05-24 21:47:06 +0800

    Complete bfs and dfs
    Finished implementing Breath-First search and Depth-First search method.

--- MIT6.034/lab2/lab2.py
+++ MIT6.034/lab2/lab2.py
@@ -7,28 +7,28 @@
 
 # 1: True or false - Hill Climbing search is guaranteed to find a solution
 #    if there is a solution
-ANSWER1 = None
+ANSWER1 = False
 
 # 2: True or false - Best-first search will give an optimal search result
 #    (shortest path length).
 #    (If you don't know what we mean by best-first search, refer to
 #     http://courses.csail.mit.edu/6.034f/ai3/ch4.pdf (page 13 of the pdf).)
-ANSWER2 = None
+ANSWER2 = False
 
 # 3: True or false - Best-first search and hill climbing make use of
 #    heuristic values of nodes.
-ANSWER3 = None
+ANSWER3 = True
 
 # 4: True or false - A* uses an extended-nodes set.
-ANSWER4 = None
+ANSWER4 = True
 
 # 5: True or false - Breadth first search is guaranteed to return a path
 #    with the shortest number of nodes.
-ANSWER5 = None
+ANSWER5 = True
 
 # 6: True or false - The regular branch and bound uses heuristic values
 #    to speed up the search for an optimal path.
-ANSWER6 = None
+ANSWER6 = False
 
 # Import the Graph data structure from 'search.py'
 # Refer to search.py for documentation
@@ -38,15 +38,67 @@ from search import Graph
 # If you implement these, the offline tester will test them.
 # If you don't, it won't.
 # The online tester will not test them.
-
 def bfs(graph, start, goal):
-    raise NotImplementedError
+    if start == goal:
+        return [start]
+    else:
+        # Form a one-element queue consisting of a zero-length path that contains
+        # only the root node.
+        queue = []
+        queue.append([start])
+        # Avoid extending the same node multiple times.
+        extended = set()
+        extended.add(start)
+        # Until the first path in the queue terminates at the goal node or the queue is empty
+        while len(queue) > 0:
+            # Remove the first path from the queue
+            path = queue.pop(0)
+            # The terminal node
+            node = path.pop()
+            # Extending the first path to all the neighbors of the terminal node
+            for neighbour in graph.get_connected_nodes(node):
+                # Reject all new paths with loops
+                if neighbour not in extended:
+                    if neighbour == goal:
+                        # The goal node is found
+                        return path + [node, neighbour]
+                    else:
+                        extended.add(neighbour)
+                        # Add new paths, if any, to the back of the queue
+                        queue.append(path + [node, neighbour])
+        return []
 
 ## Once you have completed the breadth-first search,
 ## this part should be very simple to complete.
 def dfs(graph, start, goal):
-    raise NotImplementedError
-
+    if start == goal:
+        return [start]
+    else:
+        # Form a one-element queue consisting of a zero-length path that contains
+        # only the root node.
+        queue = []
+        queue.append([start])
+        # Avoid extending the same node multiple times.
+        extended = set()
+        extended.add(start)
+        # Until the first path in the queue terminates at the goal node or the queue is empty
+        while len(queue) > 0:
+            # Remove the first path from the queue
+            path = queue.pop(0)
+            # The terminal node
+            node = path.pop()
+            # Extending the first path to all the neighbors of the terminal node
+            for neighbour in graph.get_connected_nodes(node):
+                # Reject all new paths with loops
+                if neighbour not in extended:
+                    if neighbour == goal:
+                        # The goal node is found
+                        return path + [node, neighbour]
+                    else:
+                        extended.add(neighbour)
+                        # Add new paths, if any, to the front of the queue
+                        queue.insert(0,  path + [node, neighbour])
+        return []
 
 ## Now we're going to add some heuristics into the search.  
 ## Remember that hill-climbing is a modified version of depth-first search.
